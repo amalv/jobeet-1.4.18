93236b0b88fbcf3bbe734cda812c5185c4738692
amalv <amarino@captalis.com>
2012-08-24 22:17:28 +0200

    Added getActiveJobs method to the model

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -7,6 +7,13 @@
  */
 class JobeetJobTable extends Doctrine_Table
 {
+    public function getActiveJobs()
+    {
+      $q = $this->createQuery('j')
+        ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
+   
+      return $q->execute();
+    }
     /**
      * Returns an instance of this class.
      *
