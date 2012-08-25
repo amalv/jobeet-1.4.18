e6fbe25d5bdaed9ff13038048a3fdfbe721ebed7
amalv <amarino@captalis.com>
2012-08-25 19:29:28 +0200

    Added a limit of 10 jobs to getActiveMethods in JobeetCategory doctrine class

--- lib/model/doctrine/JobeetCategory.class.php
+++ lib/model/doctrine/JobeetCategory.class.php
@@ -12,12 +12,13 @@
  */
 class JobeetCategory extends BaseJobeetCategory
 {
-    public function getActiveJobs()
+    public function getActiveJobs($max = 10)
     {
-          $q = Doctrine_Query::create()
-                  ->from('JobeetJob j')
-                      ->where('j.category_id = ?', $this->getId());
-           
-            return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
+      $q = Doctrine_Query::create()
+        ->from('JobeetJob j')
+        ->where('j.category_id = ?', $this->getId())
+        ->limit($max);
+     
+      return Doctrine_Core::getTable('JobeetJob')->getActiveJobs($q);
     }
 }
