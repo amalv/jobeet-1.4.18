91641a0cdc48f1e80231637daea8f1836bb65521
amalv <amarino@captalis.com>
2012-08-25 19:24:57 +0200

    Merged doctrine_query object with its own query in getActiveJobs methods from JobeetJobTable class

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -7,13 +7,20 @@
  */
 class JobeetJobTable extends Doctrine_Table
 {
-    public function getActiveJobs()
+    public function getActiveJobs(Doctrine_Query $q = null)
     {
-      $q = $this->createQuery('j')
-        ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()))
-        ->orderBy('j.expires_at DESC');
+      if (is_null($q))
+      {
+        $q = Doctrine_Query::create()
+          ->from('JobeetJob j');
+      }
+     
+      $q->andWhere('j.expires_at > ?', date('Y-m-d H:i:s', time()))
+        ->addOrderBy('j.expires_at DESC');
+     
       return $q->execute();
     }
+
     /**
      * Returns an instance of this class.
      *
