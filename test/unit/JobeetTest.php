4729213566dfbb0f895ff4c7a546b18e21467558
amalv <amarino@captalis.com>
2012-08-26 22:27:21 +0200

    Changed test for accents in JobeetTest

--- test/unit/JobeetTest.php
+++ test/unit/JobeetTest.php
@@ -14,7 +14,7 @@ $t->is(Jobeet::slugify(''), 'n-a', '::slugify() converts the empty string to n-a
 $t->is(Jobeet::slugify(' - '), 'n-a', '::slugify() converts a string that only contains non-ASCII characters to n-a');
 if (function_exists('iconv'))
 {
-  $t->is(Jobeet::slugify('DÃ©veloppeur Web'), 'developpeur-web', '::slugify() removes accents');
+  $t->is(Jobeet::slugify('Développeur Web'), 'developpeur-web', '::slugify() removes accents');
 }
 else
 {
