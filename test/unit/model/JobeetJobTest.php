ed0cfd6be5e9a301448279efd969c7554dd625ca
amalv <amarino@captalis.com>
2012-08-26 22:38:42 +0200

    Created new test for JobeetJob class

---
+++ test/unit/model/JobeetJobTest.php
@@ -0,0 +1,7 @@
+<?php
+include(dirname(__FILE__).'/../../bootstrap/Doctrine.php');
+ 
+$t = new lime_test(1);
+$t->comment('->getCompanySlug()');
+$job = Doctrine_Core::getTable('JobeetJob')->createQuery()->fetchOne();
+$t->is($job->getCompanySlug(), Jobeet::slugify($job->getCompany()), '->getCompanySlug() return the slug for the company');
