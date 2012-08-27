f527e28d3c0fb2510a90bc44c165458208d479fd
amalv <amarino@captalis.com>
2012-08-27 10:48:48 +0200

    Fixed number of tests expected in lime_test constructor for JobeetJobTest

--- test/unit/model/JobeetJobTest.php
+++ test/unit/model/JobeetJobTest.php
@@ -1,10 +1,11 @@
 <?php
 include(dirname(__FILE__).'/../../bootstrap/Doctrine.php');
  
-$t = new lime_test(2);
+$t = new lime_test(3);
 $t->comment('->getCompanySlug()');
 $job = Doctrine_Core::getTable('JobeetJob')->createQuery()->fetchOne();
 $t->is($job->getCompanySlug(), Jobeet::slugify($job->getCompany()), '->getCompanySlug() return the slug for the company');
+
 $t->comment('->save()');
 $job = create_job();
 $job->save();
