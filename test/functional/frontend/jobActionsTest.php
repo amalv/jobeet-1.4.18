ab020c96fa36d7d5136e9d1a95f84f3a428d1112
amalv <amarino@captalis.com>
2012-08-28 11:35:05 +0200

    Added two new tests to jobActionsTest
    - A non-existent job forwards the user to a 404.
    - An expired job page forwards the user to a 404.

--- test/functional/frontend/jobActionsTest.php
+++ test/functional/frontend/jobActionsTest.php
@@ -15,16 +15,15 @@ $browser->info('1 - The homepage')->
     checkElement('.jobs td.position:contains("expired")', false)->
   end()
 ;
-
+ 
 $max = sfConfig::get('app_max_jobs_on_homepage');
  
 $browser->info('1 - The homepage')->
-  get('/')->
   info(sprintf('  1.2 - Only %s jobs are listed for a category', $max))->
   with('response')->
     checkElement('.category_programming tr', $max)
 ;
-
+ 
 $browser->info('1 - The homepage')->
   get('/')->
   info('  1.3 - A category has a link to the category page only if too many jobs')->
@@ -33,26 +32,14 @@ $browser->info('1 - The homepage')->
     checkElement('.category_programming .more_jobs')->
   end()
 ;
-
-$q = Doctrine_Query::create()
-  ->select('j.*')
-  ->from('JobeetJob j')
-  ->leftJoin('j.JobeetCategory c')
-  ->where('c.slug = ?', 'programming')
-  ->andWhere('j.expires_at > ?', date('Y-m-d', time()))
-  ->orderBy('j.created_at DESC');
- 
-$job = $q->fetchOne();
  
 $browser->info('1 - The homepage')->
-  get('/')->
   info('  1.4 - Jobs are sorted by date')->
   with('response')->begin()->
-    checkElement(sprintf('.category_programming tr:first a[href*="/%d/"]',
-      $browser->getMostRecentProgrammingJob()->getId()))->
+    checkElement(sprintf('.category_programming tr:first a[href*="/%d/"]', $browser->getMostRecentProgrammingJob()->getId()))->
   end()
 ;
-
+ 
 $job = $browser->getMostRecentProgrammingJob();
  
 $browser->info('2 - The job page')->
@@ -67,5 +54,13 @@ $browser->info('2 - The job page')->
     isParameter('location_slug', $job->getLocationSlug())->
     isParameter('position_slug', $job->getPositionSlug())->
     isParameter('id', $job->getId())->
-  end()
+  end()->
+ 
+  info('  2.2 - A non-existent job forwards the user to a 404')->
+  get('/job/foo-inc/milano-italy/0/painter')->
+  with('response')->isStatusCode(404)->
+ 
+  info('  2.3 - An expired job page forwards the user to a 404')->
+  get(sprintf('/job/sensio-labs/paris-france/%d/web-developer', $browser->getExpiredJob()->getId()))->
+  with('response')->isStatusCode(404)
 ;
