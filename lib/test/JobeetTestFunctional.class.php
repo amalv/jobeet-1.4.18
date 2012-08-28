cc46a3ba98d380a2cb2ab13820e335c096503073
amalv <amarino@captalis.com>
2012-08-28 11:33:38 +0200

    Added getExpiredJob method to JobeetTestFuncional class

--- lib/test/JobeetTestFunctional.class.php
+++ lib/test/JobeetTestFunctional.class.php
@@ -1,6 +1,13 @@
 <?php
 class JobeetTestFunctional extends sfTestFunctional
 {
+  public function loadData()
+  {
+    Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/fixtures');
+ 
+    return $this;
+  }
+ 
   public function getMostRecentProgrammingJob()
   {
     $q = Doctrine_Query::create()
@@ -12,11 +19,13 @@ class JobeetTestFunctional extends sfTestFunctional
  
     return $q->fetchOne();
   }
-
-  public function loadData()
-  {
-    Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/fixtures');
  
-    return $this;
+  public function getExpiredJob()
+  {
+    $q = Doctrine_Query::create()
+      ->from('JobeetJob j')
+      ->where('j.expires_at < ?', date('Y-m-d', time()));
+ 
+    return $q->fetchOne();
   }
 }
