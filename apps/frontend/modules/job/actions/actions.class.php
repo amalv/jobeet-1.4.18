b7c7af86dac2c7428d405f042fe4074d0ca055ad
amalv <amarino@captalis.com>
2012-08-31 13:30:45 +0200

    Added new executePublish method to jobActions class

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -10,6 +10,18 @@
  */
 class jobActions extends sfActions
 {
+  public function executePublish(sfWebRequest $request)
+  {
+    $request->checkCSRFProtection();
+
+    $job = $this->getRoute()->getObject();
+    $job->publish();
+
+    $this->getUser()->setFlash('notice', sprintf('Your job is now online for %s days.', sfConfig::get('app_active_days')));
+
+    $this->redirect('job_show_user', $job);
+  }
+
   public function executeIndex(sfWebRequest $request)
   {
     $this->categories = Doctrine_Core::getTable('JobeetCategory')->getWithJobs();
