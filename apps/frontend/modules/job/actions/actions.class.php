7380a9dcdc7ae7004ca9b6bb131799012dc2729c
amalv <amarino@captalis.com>
2012-08-31 12:22:21 +0200

    Fixed bug not showing index for lacking executeIndex method

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -10,51 +10,61 @@
  */
 class jobActions extends sfActions
 {
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->categories = Doctrine_Core::getTable('JobeetCategory')->getWithJobs();
+  }
+
+  public function executeShow(sfWebRequest $request)
+  {
+    $this->job = $this->getRoute()->getObject();
+  }
+
   public function executeNew(sfWebRequest $request)
   {
     $this->form = new JobeetJobForm();
   }
-   
+
   public function executeCreate(sfWebRequest $request)
   {
     $this->form = new JobeetJobForm();
     $this->processForm($request, $this->form);
     $this->setTemplate('new');
   }
-   
+
   public function executeEdit(sfWebRequest $request)
   {
     $this->form = new JobeetJobForm($this->getRoute()->getObject());
   }
-   
+
   public function executeUpdate(sfWebRequest $request)
   {
     $this->form = new JobeetJobForm($this->getRoute()->getObject());
     $this->processForm($request, $this->form);
     $this->setTemplate('edit');
   }
-   
+
   public function executeDelete(sfWebRequest $request)
   {
     $request->checkCSRFProtection();
-   
+
     $job = $this->getRoute()->getObject();
     $job->delete();
-   
+
     $this->redirect('job/index');
   }
-   
+
   protected function processForm(sfWebRequest $request, sfForm $form)
   {
     $form->bind(
-      $request->getParameter($form->getName()),
-      $request->getFiles($form->getName())
-    );
-   
+        $request->getParameter($form->getName()),
+        $request->getFiles($form->getName())
+        );
+
     if ($form->isValid())
     {
       $job = $form->save();
-   
+
       $this->redirect('job_show', $job);
     }
   }
