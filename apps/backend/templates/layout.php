7ab11a99d96ab34f03d174626f9774aa87084bc6
amalv <amarino@captalis.com>
2012-09-28 15:27:33 +0200

    Added logout link in the menu for authenticated users

--- apps/backend/templates/layout.php
+++ apps/backend/templates/layout.php
@@ -21,12 +21,9 @@
     <?php if ($sf_user->isAuthenticated()): ?>
       <div id="menu">
         <ul>
-          <li>
-            <?php echo link_to('Jobs', 'jobeet_job') ?>
-          </li>
-          <li>
-            <?php echo link_to('Categories', 'jobeet_category') ?>
-          </li>
+          <li><?php echo link_to('Jobs', 'jobeet_job') ?></li>
+          <li><?php echo link_to('Categories', 'jobeet_category') ?></li>
+          <li><?php echo link_to('Logout', 'sf_guard_signout') ?></li>
         </ul>
       </div>
     <?php endif ?>
